b99ef8e5dfe8c934877c48e8c44fbfe3397533b0
dadesp <damien.despres@magellium.fr>
2020-05-14 21:57:52 +0200

    gestion du departement 69

--- pg_views/v_departements_polygon.sql
+++ pg_views/v_departements_polygon.sql
@@ -1,6 +1,11 @@
 ------------
 --departements--
 -------------
+-- le département du rhone tel qu'il est representé 
+-- sur le geoportail IGN n'est plus un département mais une circonscription départementale
+-- dans OSM ce niveau est le niveau 5 
+-- On choisi donc de faire une exception pour le rhone et de prendre le niveau 5 pour coller à la representation usuelle 
+-- des départements même si le departement 69 a été à priori remplacé par les départements 69M et 69D 
 
 DROP MATERIALIZED VIEW IF EXISTS departements_polygon;
 CREATE MATERIALIZED VIEW departements_polygon AS 
@@ -19,5 +24,10 @@ CREATE MATERIALIZED VIEW departements_polygon AS
 		tags->'osm_timestamp' AS "osm_timestamp",
 		way AS "the_geom"
 	FROM planet_osm_polygon 
-	WHERE boundary ='administrative' and admin_level='6' and tags->'ref:INSEE' is not null;
+	WHERE boundary ='administrative' and 
+	( (admin_level='6' and not tags->'ref:INSEE' like '69%') 
+		or (admin_level='5' and tags->'ref:INSEE' like '69%') 
+	)
+	
+	and tags->'ref:INSEE' is not null;
 CREATE INDEX departements_polygon_gist ON departements_polygon USING gist (the_geom);
